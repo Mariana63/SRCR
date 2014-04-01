43b74aae9bbfcae8951ed89f51ef8dc0d5671e70
Miguel Pinto <mcpinto98@gmail.com>
2014-04-01 16:07:27 +0100

    mais algumas extensões de predicado

--- trab.pl
+++ trab.pl
@@ -1,6 +1,23 @@
-:- dynamic pastelaria/2.
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: definicoes iniciais
+:- op( 900,xfy,'::' ).
+:- dynamic '-'/1.
+:- dynamic pastelaria/1.
 :- dynamic zona/2.
 :- dynamic local/2.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado pastelaria: Nome -> {V,F}
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Base de conhecimento inicial
 
 pastelaria('diana').
 pastelaria('mario').
@@ -29,3 +46,57 @@ loop(N, Sqrt, Odd, Sum, Ans) :-
 loop(N, Sqrt, _, Sum, Sqrt) :- Sum > N.
 
 distancia(X,Y,A,B,R) :- R is sqrt(((X-A)*(X-A))+((Y-B)*(Y-B))).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que não permite a insercao de conhecimento repetido
+
++pastelaria( P ) :: (solucoes( P, (pastelaria( P )), S),
+					comprimento( S,N ), N==1
+					).
+
+-pastelaria( P ) :: (
+					).
+
+-zona( P,R ) :- nao(zona( P,R )).
+-local( P,XX,YY ) :- nao(local( P,XX,YY )).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que permite a remocao do conhecimento
+
+remocao( Termo ) :-
+ 	solucoes( Invariante,-Termo::Invariante,Lista ),
+ 	teste( Lista ),
+    remover( Termo ).
+
+remover( Termo ) :-
+	retract( Termo ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que permite a evolucao do conhecimento
+
+evolucao( Termo ) :-
+    solucoes( Invariante,+Termo::Invariante,Lista ),
+    insercao( Termo ),
+    teste( Lista ).
+
+insercao( Termo ) :-
+	assert( Termo ).
+insercao( Termo ) :-
+	retract( Termo ),!,fail.
+
+teste( [] ).
+teste( [A|B] ) :-
+	A,
+	teste( B ).
+
+solucoes( X,Y,Z ) :-
+	findall( X,Y,Z ).
+
+comprimento( S,N ) :-
+	length( S,N ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado nao: Questao -> {V,F}
+
+nao(X) :- X,!,fail.
+nao(X).
