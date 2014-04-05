5520eab0a258e1f4289c79712eac069d30c4c7ca
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-04-05 19:50:17 +0100

    aleatoriadades

--- srcr1.pl
+++ srcr1.pl
@@ -18,19 +18,88 @@
 :- dynamic custoMinimo/4.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% 0- normal, 1- express, 2- ambas ????
+tipoEntrega('Viana_Castelo', 0).
+tipoEntrega('Braga', 2).
+tipoEntrega('Vila_Real', 1).
+tipoEntrega('Braganca', 2).
+tipoEntrega('Porto', 1).
+tipoEntrega('Aveiro', 2).
+tipoEntrega('Viseu', 2).
+tipoEntrega('Guarda', 1).
+tipoEntrega('Coimbra', 1).
+tipoEntrega('Castelo_Branco', 0).
+tipoEntrega('Leiria', 0).
+tipoEntrega('Santarem', 1).
+tipoEntrega('Portalegre',0).
+tipoEntrega('Lisboa', 2).
+tipoEntrega('Setubal', 1).
+tipoEntrega('Evora', 1).
+tipoEntrega('Beja', 2).
+tipoEntrega('Faro', 0).
 
-tipoEntrega('braga',0).
-tipoEntrega('lisboa',1).
-tipoEntrega('aveiro',2).
 
-localizacao('braga', (2,1)).
-localizacao('porto', (2,5)).
-localizacao('lisboa', (1,6)).
-localizacao('aveiro', (1,9)).
-localizacao('coimbra', (1,5)).
+localizacao('Viana_Castelo', (8,33)).
+localizacao('Braga', (9,31)).
+localizacao('Vila_Real', (11,30)).
+localizacao('Braganca', (15,33)).
+localizacao('Porto', (6,28)).
+localizacao('Aveiro', (6,25)).
+localizacao('Viseu', (10,25)).
+localizacao('Guarda', (14,24)).
+localizacao('Coimbra', (8,22)).
+localizacao('Castelo_Branco', (12,19)).
+localizacao('Leiria', (6,19)).
+localizacao('Santarem', (7,16)).
+localizacao('Lisboa', (5,12)).
+localizacao('Portalegre', (12,15)).
+localizacao('Setubal', (7,11)).
+localizacao('Evora', (10,12)).
+localizacao('Beja', (10,8)).
+localizacao('Faro', (10,2)).
 
-zona('braga','norte').
-zona('lisboa','centro').
+
+
+zona('Viana_Castelo', 'Norte').
+zona('Braga', 'Norte').
+zona('Vila_Real', 'Norte').
+zona('Braganca','Norte').
+zona('Porto', 'Norte').
+zona('Aveiro', 'Centro').
+zona('Viseu', 'Centro').
+zona('Guarda', 'Centro').
+zona('Coimbra', 'Centro').
+zona('Castelo_Branco', 'Centro').
+zona('Leiria', 'Centro').
+zona('Lisboa', 'Centro').
+zona('Santarem', 'Centro').
+zona('Portalegre', 'Sul').
+zona('Setubal', 'Sul').
+zona('Evora', 'Sul').
+zona('Beja', 'Sul').
+zona('Faro', 'Sul').
+
+
+sigla('Aveiro', 'A').
+sigla('Beja', 'BJ').
+sigla('Braga', 'BRG').
+sigla('Braganca', 'BG').
+sigla('Castelo_Branco', 'CB').
+sigla('Coimbra', 'C'.
+sigla('Evora', 'E').
+sigla('Faro', 'F').
+sigla('Guarda', 'G').
+sigla('Leiria', 'LR').
+sigla('Lisboa', 'LX').
+sigla('Portalegre', 'PL').
+sigla('Porto', 'PT').
+sigla('Santarem', 'STR').
+sigla('Setubal', 'ST').
+sigla('Viana_Castelo','VC').
+sigla('Vila_Real', 'VR').
+sigla('Viseu', 'V').
+
+
 
 ligacao(braga, porto,R) :- distancia(5,9,3,7,R).
 ligacao(porto, coimbra,5).
@@ -40,9 +109,6 @@ ligacao(aveiro, lisboa, 6).
 ligacao(coimbra, lisboa,600).
 ligacao(leiria, lisboa, 4).
 
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Calcula Distancia entre 2 pontos
-distancia(X,Y,A,B,R) :- R is sqrt(((X-A)*(X-A))+((Y-B)*(Y-B))).
 
 %------------------------------------------------------------- FUNÇÕES ----------------------------------------------
 
@@ -60,7 +126,11 @@ distanciaAux((X1,Y1),(X2,Y2),R) :- R is sqrt(((X1-X2)*(X1-X2))+((Y1-Y2)*(Y1-Y2))
 distancia(O, D, R) :- hacaminho(O, D), procuraLocalizacao(O, RO), procuraLocalizacao(D, RD), distanciaAux(RO, RD, R).
 
 
-%retorna sim ou não conforme houver caminha entre os pontos A e B.
+
+
+
+
+%retorna sim ou não conforme houver caminho entre os pontos A e B.
 hacaminho(A, B) :- ligacao(A, B), !.
 hacaminho(A, B) :- ligacao(A, X), hacaminho(X, B).
 
@@ -69,8 +139,7 @@ hacaminho(A, B) :- ligacao(A, X), hacaminho(X, B).
 %------------------------------------------------------------- FUNÇÕES ----------------------------------------------
 
 ha_caminho(A, B) :- ligacao(A, B, _), !.
-ha_caminho(A, B) :- ligacao(A, X, _),
-ha_caminho(X, B).
+ha_caminho(A, B) :- ligacao(A, X, _), ha_caminho(X, B).
 
 travessia(A, B, Visitados, [B|Visitados]) :- ligacao(A, B, _).
 travessia(A, B, Visitados, Cam) :- ligacao(A, C, _),
