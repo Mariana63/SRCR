8d712181a93083e8fea35eba8178d71364152c56
Miguel Pinto <mcpinto98@gmail.com>
2014-05-12 00:48:01 +0100

    final

--- Trabalho2/srcr.pl
+++ Trabalho2/srcr.pl
@@ -7,8 +7,8 @@
 :- dynamic farmacia/2.
 :- dynamic armazem/2.
 :- dynamic medicamento/2.
-:- dynamic data_colcoacao_mercado/5.
-:- dynamic data_validade/5.
+:- dynamic data_colocacao_mercado/4.
+:- dynamic data_validade/4.
 :- dynamic aplicacao_clinica/2.
 :- dynamic apresentacao_farmaceutica/2.
 :- dynamic preco_pvp/3.
@@ -73,9 +73,9 @@ medicamento('Ben-u-ron', incerto).
                     ).
                    
 %%  Conhecimento negativo %%
--medicamento(M,P) :- nao(medicamento(M,P)), nao(excepcao(medicamento(M,P))).
+-medicamento(M,P) :- nao(medicamento(M,P)), nao(excecao(medicamento(M,P))).
 
-excepcao(medicamento('Voltaren',interdito)).
+excecao(medicamento('Voltaren',interdito)).
 +medicamento(M,PA) :: (solucoes(PA, (medicamento('Voltaren',PA), nao(nulo(V))), [])).
 
 %------------------------------------------------------------------------- DATA COLOCACAO MERCADO ----------------------------------------------------------------
@@ -84,94 +84,62 @@ data_colocacao_mercado('Daflon', incerto, 05, 2014).
 data_colocacao_mercado('Calde', 01, 04, 2013).
 data_colocacao_mercado('Fagico', 01, 01, 2012).
 data_colocacao_mercado('Egide', 10, 6, 2012).
-data_colocacao_mercado('Magrix', 03, 02, 2012).
 data_colocacao_mercado('Oceral', 10, 06, 2002).
 data_colocacao_mercado('Parasin', incerto, 02, 1992).
-data_colocacao_mercado('Pantozol', 11, 06, 2012).
+data_colocacao_mercado('Pantozol', data_nula, data_nula, data_nula).
 
-%% Invariantes 
+%-- Invariantes 
++data_colocacao_mercado(X,D,M,A) :: medicamento(X,_).                                
++data_colocacao_mercado(X,D,M,A) :: (solucoes(X, data_colocacao_mercado( X,D,M,A ), S),
+                                    comprimento( S,N ), N == 1
+                                    ).
++data_colocacao_mercado(X,D,M,A) :: (D>=0; D=<31; M>=0; M=<12; A>=0).                              
 
-% Não há conhecimento repetido %
-+data_colocacao_mercado(X,D,M,A) :: (solucoes((X,D,M,A), data_colocacao_mercado( X,D,M,A ), S),
-                                comprimento( S,N ), N == 1
-                                ).
+%%-- Conhecimento negativo
+-data_colocacao_mercado(X,D,M,A) :- nao(data_colocacao_mercado(X,D,M,A)), nao(excecao(data_colocacao_mercado(X,D,M,A))).
 
-% %
-+data_colocacao_mercado(X,D,M,A) :: (medicamento(X)).
+%%-- Exceções
+excecao(data_colocacao_mercado('Magrix',D,M,A)) :- D>4, D<13, M>3, M<5, A>2013, A<2015.
 
-
-% Data de validade é superior à data de inserção %
-+data_colocacao_mercado( X,D,M,A ) :: (
-                                ( nao(data_validade(X,DI,MI,AI)) );
-                                ( A>AI );
-                                ( A==AI, M>MI );
-                                ( A==AI, M==MI, D>DI )).
-                                
-+data_colocacao_mercado(X,D,M,A) :: (findall((Dia, Mes, Ano), (data_colocacao_mercado(X, Dia, Mes, Ano),
-                                      (nao(nulo(Dia)); nao(nulo(Mes)); nao(nulo(Ano)))), [])).
-                                      
-%%   Conhecimento negativo
--data_colocacao_mercado(X,D,M,A) :- nao(data_colocacao_mercado(X,D,M,A)), nao(excepcao(data_colocacao_mercado(X,D,M,A))).
-
-
-%%   Exceções
-excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(X, data_nula, data_nula, data_nula).
-                                
 excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(X,incerto,M,A).
 excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(X,D,incerto,A).
 excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(X,D,M,incerto).
 excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(incerto,D,M,A).
 excecao(data_colocacao_mercado(X,D,M,A)) :- data_colocacao_mercado(X,incerto,incerto,incerto).
-
-                               
-                                
+                     
 %------------------------------------------------------------------------------ DATA VALIDADE ----------------------------------------------------------------------
 %Extensao do predicado data_validade: Nome Medicamento, Data(dia, mes, ano) -> {V,F,D}
 
 data_validade('Daflon', 01, 01, 2016).
-data_validade('Calde', 01, 02, 2017).
 data_validade('Fagico', 04, 09, 2015).
 data_validade('Egide', 01, 01, 2019).
 data_validade('Magrix', incerto, 05, 2015).
 data_validade('Oceral', 31, 12, 2015).
-data_validade('Parasin', 15, 06, 2020).
+data_validade('Parasin', data_nula, data_nula, data_nula).
 data_validade('Pantozol', 11, 06, 2016).
 
-
-%% Invariantes 
-
-% Não há conhecimento repetido %
-+data_validade(X,D,M,A) :: (solucoes((X,D,M,A), data_validade( X,D,M,A ), S),
-                                comprimento( S,N ), N == 1
-                                ).
-
-%%
-+data_validade(X,D,M,A) :: (medicamento(X)).
-
-
-% Data de validade é superior à data de inserção %
+%% Invariantes
++data_validade(X,D,M,A) :: (medicamento(X,_)).
++data_validade(X,D,M,A) :: (solucoes(X, data_validade( X,D,M,A ), S),
+                            comprimento( S,N ), N == 1
+                          ).
 +data_validade( X,D,M,A ) :: (
-                                ( nao(data_colocacao_mercado(X,DI,MI,AI)) );
-                                ( A>AI );
-                                ( A==AI, M>MI );
-                                ( A==AI, M==MI, D>DI )).
-
-+data_validade(X,D,M,A) :: (solucoes((Dia, Mes, Ano), (data_validade(X, Dia, Mes, Ano)),
-                                     (nao(nulo(Dia)), nao(nulo(Mes)), nao(nulo(Ano)))), []).
+                             ( (data_colocacao_mercado(X,DI,MI,AI)) );
+                             ( A>AI );
+                             ( A==AI, M>MI );
+                             ( A==AI, M==MI, D>DI )).
                  
 %%   Conhecimento negativo    %%
--data_validade(X,D,M,A) :- nao(data_validade(X,D,M,A)), nao(excepcao(data_validade(X,D,M,A))).
+-data_validade(X,D,M,A) :- nao(data_validade(X,D,M,A)), nao(excecao(data_validade(X,D,M,A))).
 
 %%   Excepções    %%
-excepcao(data_validade(X,D,M,A)) :- data_validade(X, data_nula, data_nula, data_nula).
+excecao(data_validade('Calde',03,10,A)) :- A>2001, A<2006.
 
-excepcao(data_validade(X,D,M,A)) :- data_validade(X,incerto,M,A).
-excepcao(data_validade(X,D,M,A)) :- data_validade(X,D,incerto,A).
-excepcao(data_validade(X,D,M,A)) :- data_validade(X,D,M,incerto).
-excepcao(data_validade(X,D,M,A)) :- data_validade(incerto,D,M,A).
-excepcao(data_validade(X,D,M,A)) :- data_validade(X,incerto,incerto,incerto).
-
-
+excecao(data_validade(X,D,M,A)) :- data_validade(X,incerto,M,A).
+excecao(data_validade(X,D,M,A)) :- data_validade(X,D,incerto,A).
+excecao(data_validade(X,D,M,A)) :- data_validade(X,D,M,incerto).
+excecao(data_validade(X,D,M,A)) :- data_validade(incerto,D,M,A).
+excecao(data_validade(X,D,M,A)) :- data_validade(X,incerto,incerto,incerto).
 
 %-------------------------------------------------------------------------- APLICACAO CLINICA --------------------------------------------------------------------
 %Extensao do predicado aplicacao_clinica: Nome Medicamento, Aplicacoes Clinicas -> {V,F,D}
@@ -180,89 +148,81 @@ aplicacao_clinica('Calde', 'suplemento').
 aplicacao_clinica('Fagico', 'distúrbios gastrointestinais').
 aplicacao_clinica('Egide', 'epilepsia').
 aplicacao_clinica('Magrix', 'reduz colesterol e gordura').
-aplicacao_clinica('Oceral', 'dermatofitoses').
+aplicacao_clinica('Oceral', incerto).
 aplicacao_clinica('Parasin', 'elimina parasitas').
 aplicacao_clinica('Pantozol', 'ulceras').
 
-
-
-
 %% Invariantes %%
++aplicacao_clinica(M,AC) :: medicamento(M,_).
 +aplicacao_clinica(M,A) :: (solucoes((M,A), (aplicacao_clinica(M,A)),S), comprimento(S,N), N == 1).
                 
-
 %% Conhecimento negativo %%
--aplicacao_clinica(M,A) :- nao(aplicacao_clinica(M,A)), nao(excepcao(aplicacao_clinica(M,A))).
-
+-aplicacao_clinica(M,A) :- nao(aplicacao_clinica(M,A)), nao(excecao(aplicacao_clinica(M,A))).
 
 %% Excecoes %%
-excepcao(aplicacao_clinica(M,A)) :- aplicacao_clinica(M, incerto). 
-excepcao(aplicacao_clinica(M,A)) :- aplicacao_clinica(incerto, A).
-excepcao(aplicacao_clinica(A,B)) :- aplicacao_clinica(A, aplicacao_clinica_nula).
+excecao(aplicacao_clinica(M,A)) :- aplicacao_clinica(M, incerto). 
+excecao(aplicacao_clinica(M,A)) :- aplicacao_clinica(incerto, A).
+excecao(aplicacao_clinica(A,B)) :- aplicacao_clinica(A, aplicacao_clinica_nula).
 
-
-
+-aplicacao_clinica('Pantozol', 'Pes').
 
 %--------------------------------------------------------------------------- APRESENTACAO FARMACEUTICA -----------------------------------------------------------------
 %Extensao do predicado apresentacao_farmaceutica: Nome Medicamento, Apresentacao Farmaceutica -> {V,F,D}
-
 apresentacao_farmaceutica('Calde', 'comprimidos').
+apresentacao_farmaceutica('Calde', 'supositorio').
 apresentacao_farmaceutica('Fagico', 'xarope').
-apresentacao_farmaceutica('Egide', 'comprimidos').
+apresentacao_farmaceutica('Egide', incerto).
 apresentacao_farmaceutica('Magrix', 'comprimidos').
+apresentacao_farmaceutica('Magrix', 'xarope').
 apresentacao_farmaceutica('Oceral', 'creme').
+apresentacao_farmaceutica('Oceral', 'comprimidos').
 apresentacao_farmaceutica('Parasin', 'suspensao oral').
-apresentacao_farmaceutica('Pantozol', 'comprimidos').
-
-
 
 %% Invariantes %%
-+apresentacao_farmaceutica(M,A) :: (solucoes((M,A), (apresentacao_farmaceutica(M,A)),S), comprimento(S,N), N == 1).
++apresentacao_farmaceutica(M,A) :: medicamento(M,_).
++apresentacao_farmaceutica(M,A) :: (solucoes((M,A), (apresentacao_farmaceutica(M,A)),S), comprimento(S,N), N < 4).
                 
-
-
 %% Conhecimento negativo %%
--apresentacao_farmaceutica(M,A) :- nao(apresentacao_farmaceutica(M,A)), nao(excepcao(apresentacao_farmaceutica(M,A))).
-
-
+-apresentacao_farmaceutica(M,A) :- nao(apresentacao_farmaceutica(M,A)), nao(excecao(apresentacao_farmaceutica(M,A))).
 
 %% Excecoes %%
-excepcao(apresentacao_farmaceutica(M,A)) :- apresentacao_farmaceutica(M, incerto). 
-excepcao(apresentacao_farmaceutica(M,A)) :- apresentacao_farmaceutica(incerto, A).
-excepcao(apresentacao_farmaceutica(A,B)) :- apresentacao_farmaceutica(A, apresentacao_farmaceutica_nula).
+excecao(apresentacao_farmaceutica(M,A)) :- apresentacao_farmaceutica(M, incerto). 
+excecao(apresentacao_farmaceutica(M,A)) :- apresentacao_farmaceutica(incerto, A).
+excecao(apresentacao_farmaceutica(A,B)) :- apresentacao_farmaceutica(A, apresentacao_farmaceutica_nula).
 
-
-
-
-
+excecao(apresentacao_farmaceutica('Pantozol', interdito)).
++apresentacao_farmaceutica(M,V)::(solucoes(V, (apresentacao_farmaceutica('Pantozol',V), nao(nulo(V))) ,[])).
 
 %--------------------------------------------------------------------------------- PRECO PVP -------------------------------------------------------------------------
 %Extensao do predicado preco_pvp: Nome Medicamento, Apresentacao Farmaceutica, Preco -> {V,F,D}
 preco_pvp('Daflon', 'comprimidos',80).
-preco_pvp('Calde', 'comprimidos',50).
+preco_pvp('Calde', incerto,50).
 preco_pvp('Fagico', 'xarope',12).
-preco_pvp('Egide', 'comprimidos',12).
 preco_pvp('Magrix', 'comprimidos',33).
 preco_pvp('Oceral', 'creme',20).
+preco_pvp('Oceral', 'comprimidos',incerto).
 preco_pvp('Parasin', 'suspensao oral',7).
 preco_pvp('Pantozol', 'comprimidos',52).
 
 %% invariantes
-
-+preco_pvp(M,A,P) :: (solucoes((M,A,P),preco_pvp( M,A,P ), S),
-                                comprimento( S,N ), N == 1
-                                ).
-+preco_pvp(M,A,P) :: (medicamento(M), apresentacao_farmaceutica(M,A)).
++preco_pvp(M,A,P) :: (medicamento(M,_), apresentacao_farmaceutica(M,A)).
++preco_pvp(M,A,P) :: (solucoes((M,A), (preco_pvp(M,A,P)),S), comprimento(S,N), N==1).
 
 %%   Conhecimento negativo
--preco_pvp(M,A,P) :- nao(preco_pvp(M,A,P)), nao(excepcao(preco_pvp(M,A,P))).
+-preco_pvp(M,A,P) :- nao(preco_pvp(M,A,P)), nao(excecao(preco_pvp(M,A,P))).
 
 %% Excecoes
+excecao(preco_pvp(M,A,P)) :- preco_pvp(M,A, preco_nulo).
+excecao(preco_pvp(M,A,P)) :- preco_pvp(M,incerto,P).
+excecao(preco_pvp(M,A,P)) :- preco_pvp(incerto,A,P).
+excecao(preco_pvp(M,A,P)) :- preco_pvp(M,A,incerto).
+preco_pvp('Egide', 'comprimidos',12).
 
-excepcao(preco_pvp(M,A,P)) :- preco_pvp(M,A, preco_nulo).
-excepcao(preco_pvp(M,A,P)) :- preco_pvp(M,incerto,P).
-excepcao(preco_pvp(M,A,P)) :- preco_pvp(incerto,A,P).
-excepcao(preco_pvp(M,A,P)) :- preco_pvp(M,A,incerto).
+acerca(X,Y) :- Z is X*0.95,
+               W is X*1.05,
+               Y=<W,
+               Y>=Z.
+excecao(preco_pvp('Egide', 'comprimidos',X)) :- acerca(100,X).
 
 %------------------------------------------------------------------------------- PRECO RECOMENDADO ---------------------------------------------------------------------
 %Extensao do predicado preco_recomendado: Nome Medicamento, Apresentacao Farmaceutica, Preco -> {V,F,D}
@@ -271,54 +231,57 @@ preco_recomendado('Calde', 'comprimidos',40).
 preco_recomendado('Fagico', 'xarope',10).
 preco_recomendado('Egide', 'comprimidos',10).
 preco_recomendado('Magrix', 'comprimidos',23).
-preco_recomendado('Oceral', 'creme',14).
 preco_recomendado('Parasin', 'suspensao oral',5).
 preco_recomendado('Pantozol', 'comprimidos',45).
 
 %% invariantes
-
-+preco_recomendado(M,A,P) :: (solucoes((M,A,P),preco_recomendado( M,A,P ), S),
-                                comprimento( S,N ), N == 1
-                                ).
-+preco_recomendado(M,A,P) :: (medicamento(M), apresentacao_farmaceutica(M,A)).
++preco_recomendado(M,A,P) :: (solucoes((M,A),preco_recomendado( M,A,P ), S),
+                              comprimento( S,N ), N == 1
+                              ).
++preco_recomendado(M,A,P) :: (medicamento(M,_), apresentacao_farmaceutica(M,A), preco_pvp(M,A,P)).
 
 %%   Conhecimento negativo
--preco_recomendado(M,A,P) :- nao(preco_recomendado(M,A,P)), nao(excepcao(preco_recomendado(M,A,P))).
+-preco_recomendado(M,A,P) :- nao(preco_recomendado(M,A,P)), nao(excecao(preco_recomendado(M,A,P))).
 
 %% Excecoes
 
-excepcao(preco_recomendado(M,A,P)) :- preco_recomendado(M,A, preco_nulo).
-excepcao(preco_recomendado(M,A,P)) :- preco_recomendado(M,incerto,P).
-excepcao(preco_recomendado(M,A,P)) :- preco_recomendado(incerto,A,P).
-excepcao(preco_recomendado(M,A,P)) :- preco_recomendado(M,A,incerto).
+excecao(preco_recomendado(M,A,P)) :- preco_recomendado(M,A, preco_nulo).
+excecao(preco_recomendado(M,A,P)) :- preco_recomendado(M,incerto,P).
+excecao(preco_recomendado(M,A,P)) :- preco_recomendado(incerto,A,P).
+excecao(preco_recomendado(M,A,P)) :- preco_recomendado(M,A,incerto).
+
+preco_recomendado('Oceral', 'creme',incerto).
+-preco_recomendado('Oceral', 'creme',15).
 
 %-------------------------------------------------------------------------------- REGIME ESPECIAL ----------------------------------------------------------------------
 %Extensao do predicado regime_especial: Nome Medicamento, Apresentacao Farmaceutica, Preco -> {V,F,D}
 regime_especial('Daflon', 'comprimidos',40).
 regime_especial('Calde', 'comprimidos',20).
-regime_especial('Fagico', 'xarope',5).
+regime_especial('Fagico', 'xarope',incerto).
 regime_especial('Egide', 'comprimidos',5).
 regime_especial('Magrix', 'comprimidos',20).
-regime_especial('Oceral', 'creme',10).
+regime_especial('Oceral', 'creme',incerto).
 regime_especial('Parasin', 'suspensao oral',5).
-regime_especial('Pantozol', 'comprimidos',40).
 
 %% invariantes
 
-+regime_especial(M,A,P) :: (solucoes((M,A,P),regime_especial( M,A,P ), S),
++regime_especial(M,A,P) :: (solucoes((M,A),regime_especial( M,A,P ), S),
                                 comprimento( S,N ), N == 1
                                 ).
-+regime_especial(M,A,P) :: (medicamento(M), apresentacao_farmaceutica(M,A)).
++regime_especial(M,A,P) :: (medicamento(M,_), apresentacao_farmaceutica(M,A), preco_pvp(M,A,P)).
 
 %%   Conhecimento negativo
--regime_especial(M,A,P) :- nao(regime_especial(M,A,P)), nao(excepcao(regime_especial(M,A,P))).
+-regime_especial(M,A,P) :- nao(regime_especial(M,A,P)), nao(excecao(regime_especial(M,A,P))).
 
 %% Excecoes
 
-excepcao(regime_especial(M,A,P)) :- regime_especial(M,A, preco_nulo).
-excepcao(regime_especial(M,A,P)) :- regime_especial(M,incerto,P).
-excepcao(regime_especial(M,A,P)) :- regime_especial(incerto,A,P).
-excepcao(regime_especial(M,A,P)) :- regime_especial(M,A,incerto).
+excecao(regime_especial(M,A,P)) :- regime_especial(M,A, preco_nulo).
+excecao(regime_especial(M,A,P)) :- regime_especial(M,incerto,P).
+excecao(regime_especial(M,A,P)) :- regime_especial(incerto,A,P).
+excecao(regime_especial(M,A,P)) :- regime_especial(M,A,incerto).
+
+excecao(regime_especial('Pantozol', 'comprimidos',40)).
+excecao(regime_especial('Pantozol', 'comprimidos',25)).
 
 %---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 %-------------------------------------------------------------------------------- FUNÇÕES GERAIS ---------------------------------------------------------------------------
@@ -370,5 +333,3 @@ remocao( Termo ) :-
     remover( Termo ).
 
 remover( Termo ) :- retract( Termo ).
-
-
