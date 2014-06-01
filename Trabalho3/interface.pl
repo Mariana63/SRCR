b9a7271788dc437d075c7562940f4ad46f958f90
Miguel Pinto <mcpinto98@gmail.com>
2014-06-01 19:55:32 +0100

    interface, update

--- Trabalho3/interface.pl
+++ Trabalho3/interface.pl
@@ -1,10 +1,20 @@
-:-op(900,xfy,'::').
+% Distribuicao da Computacao - LINDA
+% SICStus PROLOG: definicoes iniciais
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
 
+:- op(900,xfx,'::' ).
+:- dynamic '-'/1.
+:- dynamic p/2.
 
-demo(Agente,Questao,Resposta):-
-	out(demo(Agente,Questao)),
-	in(prova(Entidade,Resposta)).
+:- use_module(library('linda/client')).
+:-use_module(library('system')).
 
-	ligar(QN):- linda_client(QN).
+qn(L) :- bagof_rd_noblock(X,X,L).
 
-	qn(L):-bagof_rd_noblock(X,X,L);
+ligar(X):- linda_client(X).
+
+p(Agente,Questao) :- out(demo(Agente,Questao)),
+					 in(prova(X,S)),
+					 write(S),nl.
